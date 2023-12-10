67746073c7fab8cc619db1abe9cdda426fa5fe32
nijyaaa <ndf4700@students.uc-bcf.edu.ph>
2023-12-10 18:15:49 +0800

    hehehehe

--- app/src/main/java/com/example/triforce_everythinglatrinidad/MainActivity.kt
+++ app/src/main/java/com/example/triforce_everythinglatrinidad/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.triforce_everythinglatrinidad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cafeButton = findViewById<ImageButton>(R.id.cafeButton)
+        cafeButton.setOnClickListener{
+            val intent = Intent(this, CafeActivity:: class.java)
+            startActivity(intent)
+        }
+
+        val restoButton = findViewById<ImageButton>(R.id.restoButton)
+        restoButton.setOnClickListener{
+            val intent = Intent(this, RestaurantsActivity:: class.java)
+            startActivity(intent)
+        }
+
+        val spotsButton = findViewById<ImageButton>(R.id.spotsButton)
+        spotsButton.setOnClickListener{
+            val intent = Intent(this, SpotsActivity:: class.java)
+            startActivity(intent)
+        }
+
+        val hotelsButton = findViewById<ImageButton>(R.id.hotelsButton)
+        hotelsButton.setOnClickListener{
+            val intent = Intent(this, HotelsActivity:: class.java)
+            startActivity(intent)
+        }
     }
 }
