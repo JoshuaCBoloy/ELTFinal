5b9004a4a0b74aef27e4e619adda1908869cfb34
Jashqtieee <instajoshua04@gmail.com>
2023-12-11 08:06:48 +0800

    lapit na

--- app/src/main/java/com/example/triforce_everythinglatrinidad/MainActivity.kt
+++ app/src/main/java/com/example/triforce_everythinglatrinidad/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.triforce_everythinglatrinidad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         val cafeButton = findViewById<ImageButton>(R.id.cafeButton)
         cafeButton.setOnClickListener{
@@ -31,6 +38,12 @@ class MainActivity : AppCompatActivity() {
         val hotelsButton = findViewById<ImageButton>(R.id.hotelsButton)
         hotelsButton.setOnClickListener{
             val intent = Intent(this, HotelsActivity:: class.java)
+            startActivity(intent)
+        }
+
+        val highlightButton = findViewById<ImageView>(R.id.imageView5)
+        highlightButton.setOnClickListener {
+            val intent = Intent(this, kaiDetails::class.java)
             startActivity(intent)
         }
     }
